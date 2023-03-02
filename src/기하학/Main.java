39099bb4963f42a7afa6b2eed0e1ef7144615a28
이승현 <iseunghyeon@iseunghyeons-MacBook-Pro.local>
2023-03-02 17:17:48 +0900

    재귀 재귀함수가 뭔가요?

--- src/기하학/Main.java
+++ src/기하학/Main.java
@@ -1,57 +1,56 @@
 package 기하학;
 
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    /**
-     *
-     *  1. 사다리꼴 // 땡
-     *  2. 평행사변형
-     *  3. 마름모
-     *  4. 직사각형, 정사각형
-     *
-     */
+    static int[][] point = new int[4][2];
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        int x1,y1, x2,y2, x3,y3; //좌표
+        int n = Integer.parseInt(br.readLine());
 
-        // 점 A, B, C입력
-        x1 = Integer.parseInt(st.nextToken());
-        y1 = Integer.parseInt(st.nextToken());
+        Set<Integer> setX = new HashSet<>();
+        Set<Integer> setY = new HashSet<>();
+        Set<String> setXY = new HashSet<>();
+        for (int i=0; i<n; i++) {
 
-        x2 = Integer.parseInt(st.nextToken());
-        y2 = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < 4; j++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
 
-        x3 = Integer.parseInt(st.nextToken());
-        y3 = Integer.parseInt(st.nextToken());
+                setX.add(x);
+                setY.add(y);
+                setXY.add(String.valueOf(x) + String.valueOf(y));
+            }
 
+            if (setXY.size() < 4) bw.write("0\n");
+            else {
 
+                if (setX.size() >= 3) bw.write("0\n");
+                else {
 
-
-        if ((x2-x1)*(y3-y2) == (x3-x2)*(y2-y1)) {
-            bw.write("-1");
-        }
-        else {
-            double d1 = getDistance(x1, y1, x2, y2);
-            double d2 = getDistance(x1, y1, x3, y3);
-            double d3 = getDistance(x2, y2, x3, y3);
-            double max = Math.max(d1 + d2, Math.max(d1 + d3, d2 + d3));
-            double min = Math.min(d1 + d2, Math.min(d1 + d3, d2 + d3));
-
-            bw.write((max - min) * 2 + "");
+                    if (setY.size() >= 3 ) bw.write("0\n");
+                    else {
+                        bw.write("1\n");
+                    }
+                }
+            }
+            setX.clear();
+            setY.clear();
         }
 
         bw.close();
-
     }
 
-    static double getDistance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1 - y2, 2));
-    }
 }
+
+
