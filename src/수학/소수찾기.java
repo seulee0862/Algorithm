7f3441083928089e39b3dff090507e8e65bfafc2
이승현 <iseunghyeon@iseunghyeons-MacBook-Pro.local>
2023-03-07 19:37:44 +0900

    수학 소수찾기

---
+++ src/수학/소수찾기.java
@@ -0,0 +1,35 @@
+package 수학;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 소수찾기 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] primes = new int[1001];
+        primes[0] = 1;
+        primes[1] = 1;
+        for (int i = 0; i < 501; i++) {
+            if (primes[i] == 0) {
+                for (int j = i+i; j < 1001; j += i) {
+                    primes[j] = 1;
+                }
+            }
+        }
+
+        int answer = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (primes[num] == 0) answer++;
+        }
+
+        System.out.println(answer);
+    }
+}
