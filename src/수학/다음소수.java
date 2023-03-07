3d91d528d48b98075bee24654cb47aeb9cc1ff62
이승현 <iseunghyeon@iseunghyeons-MacBook-Pro.local>
2023-03-07 20:19:43 +0900

    수학 다음소수

---
+++ src/수학/다음소수.java
@@ -0,0 +1,26 @@
+package 수학;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class 다음소수 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; ++i) {
+            long testValue = Long.parseLong(br.readLine());
+            BigInteger nextPrimeNumber = new BigInteger(String.valueOf(testValue));
+
+            if (nextPrimeNumber.isProbablePrime(10)) {  // 입력 값이 소수면 그대로 출력
+                bw.write(nextPrimeNumber+"\n");
+            } else {  // 입력 값이 소수가 아니면 다음 소수를 구해서 출력
+                bw.write(nextPrimeNumber.nextProbablePrime()+"\n");
+            }
+        }
+
+        bw.close();
+    }
+}
