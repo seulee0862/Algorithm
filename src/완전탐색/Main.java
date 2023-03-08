04470f9e4e4eb1101f96ca413f76ee4636d6197a
이승현 <iseunghyeon@iseunghyeons-MacBook-Pro.local>
2023-03-08 16:20:03 +0900

    그리디 보물

---
+++ src/완전탐색/Main.java
@@ -0,0 +1,36 @@
+package 완전탐색;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int packageValue = Integer.MAX_VALUE;
+        int singleValue = Integer.MAX_VALUE;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            packageValue = Math.min(packageValue, Integer.parseInt(st.nextToken()));
+            singleValue = Math.min(singleValue, Integer.parseInt(st.nextToken()));
+        }
+
+        System.out.println(pay(N, packageValue, singleValue));
+    }
+
+    static int pay(int N, int pacakageValue, int singleValue) {
+
+        int quotient = N/6;
+        int rest = N%6;
+
+        return Math.min((quotient+1) * pacakageValue,
+                Math.min((quotient * pacakageValue) + (rest * singleValue)
+                        , singleValue * N));
+    }
+}
