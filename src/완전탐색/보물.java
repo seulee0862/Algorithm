eebe9bc56522fb339d5258b894bf765197318f6a
이승현 <iseunghyeon@iseunghyeons-MacBook-Pro.local>
2023-03-08 15:14:26 +0900

    완전탐색 보물

---
+++ src/완전탐색/보물.java
@@ -0,0 +1,38 @@
+package 완전탐색;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class 보물 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Integer[] A = new Integer[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A, Collections.reverseOrder());
+
+        int[] B = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(B);
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += A[i] * B[i];
+        }
+
+        System.out.println(sum);
+    }
+}
