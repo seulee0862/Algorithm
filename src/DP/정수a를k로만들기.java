00e9c2b12589b443b034868d480d7fd04d819419
lsh <seulee0862@gmail.com>
2023-04-27 23:18:08 +0900

    4/27 정수 a를 k로 만들기

---
+++ src/DP/정수a를k로만들기.java
@@ -0,0 +1,51 @@
+package DP;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 정수a를k로만들기 {
+
+    static BufferedReader   br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer  st;
+    static int              N, K;
+
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+
+
+
+        while (N != K) {
+            int rest = K % 2;
+            count += rest;
+            K = K - rest;
+
+            if (K == N) {
+                break;
+            }
+            if (K < 2*N) {
+                K = K - 1;
+                count++;
+                continue;
+            }
+
+            K = K/2;
+            count++;
+
+        }
+
+        System.out.println(count);
+    }
+
+    /**
+
+     *          7 77
+     *
+     *
+     *
+     */
+}
