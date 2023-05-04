9860cf172690b4b51b3af5ad4c1ec4b8d91e0e27
lsh <seulee0862@gmail.com>
2023-05-04 20:08:40 +0900

    9655 dp 돌게임

---
+++ src/DP/돌게임.java
@@ -0,0 +1,31 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 돌게임 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int[] dp = new int[1001];
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 1;
+
+        for (int i = 4; i <= N; i++) {
+            dp[i] = Math.min(dp[i-1], dp[i-3]) + 1;
+        }
+
+        if (dp[N] % 2 == 0) {
+            System.out.println("CY");
+        }
+        else {
+            System.out.println("SK");
+        }
+    }
+}
