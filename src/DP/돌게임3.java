be01a7dc37d1ec193b1af974fadcade446f32797
lsh <seulee0862@gmail.com>
2023-05-19 11:16:57 +0900

    dp 돌게임

---
+++ src/DP/돌게임3.java
@@ -0,0 +1,40 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 돌게임3 {
+
+    static BufferedReader   br = new BufferedReader(new InputStreamReader(System.in));
+    static boolean          dp[] = new boolean[1001];
+    static int              N;
+
+    public static void main(String[] args) throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+
+        dp[1] = true;
+        dp[3] = true;
+        dp[4] = true;
+        for (int i = 5; i <= N; i++) {
+            if (dp[i - 1] && dp[i - 3] && dp[i -4]) {
+                dp[i] = false;
+            }
+            else {
+                dp[i] = true;
+            }
+        }
+
+        answerPrint();
+    }
+
+    private static void answerPrint() {
+        if (dp[N]) {
+            System.out.println("SK");
+        }
+        else {
+            System.out.println("CY");
+        }
+    }
+}
